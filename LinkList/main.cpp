b05fce7a0d7d768470097d68c1e410017e1b5c3e
fwgood <fwgood@qq.com>
2016-08-16 16:16:53 +0800

    0.3.0

--- LinkList/main.cpp
+++ LinkList/main.cpp
@@ -11,13 +11,35 @@
 #define OVERFLOW -2
 #define LIST_INIT_SIZE 100
 #define LIST_INCREMENT 10
-#define elementType int
+#define ElementType int
 typedef int Status;
 typedef struct LNode{
-    elementType data;
+    ElementType data;
     struct LNode *next;
 }*Link,*Position;
 typedef struct{
     Link head,tail;
     int length;
 }LinkList;
+Status MakeNode(Link *P,ElementType e);
+void FreeNode(Link &p);
+Status InitList(LinkList *L);
+Status DestroyList(LinkList *L);
+Status ClearList(LinkList *L);
+Status InsFirst(Link h,Link s);
+Status DelFirst(Link h,Link *q);
+Status Append(LinkList *L,Link s);
+Status Remove(LinkList *l,Link *q);
+Status InsBefore(LinkList *L,Link *p,Link s);
+Status InsAfter(LinkList *L,Link *p,Link s);
+Status SetCurElem(Link *p,ElementType e);
+ElementType GetCurElem(Link *p,ElementType e);
+Status ListEmpty(LinkList L);
+int ListLength(LinkList L);
+Position GetHead(LinkList L);
+Position GetLast(LinkList L);
+Position PriorPos(LinkList L,Link p);
+Position NextPos(LinkList L,Link p);
+Status LocatePos(LinkList L,int i,Link *P);
+Position LocateElem(LinkList L,ElementType e,Status (* compare)(ElementType,ElementType));
+Status ListTraverse(LinkList L,Status (* visit)());
